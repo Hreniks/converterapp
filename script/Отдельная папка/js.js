1d552f117dde560234478d1bdc1879610f03d74f
hreniks <sbigor09@gmail.com>
2020-11-05 23:56:39 +0300

    fixed timer

--- script/Отдельная папка/js.js
+++ script/Отдельная папка/js.js
@@ -1,3 +1,5 @@
+window.addEventListener('DOMContentLoaded', function(){
+    function timer(){
 "use strict";
 let date = new Date();
 let seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
@@ -6,34 +8,13 @@ let hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
 let today = date.getDay();
 let time = date.getTime();
 let newYear = new Date('January 1, 2021 00:00:00');
-console.log(newYear);
-console.log('seconds', seconds);
-console.log('minutes', minutes);
-console.log('hours', hours);
-console.log('today', today);
-console.log('time: ', time);
-console.log('date', date);
-//switch (hours){
-//     case (hours > 5 && hours <= 12):
-//         document.body.textContent = 'Доброе утро';
-//         break;
-
-//     case (hours > 12 && hours <= 16):
-//         document.body.textContent = 'Добрый день';
-//         break;
-
-//     case (hours > 16 && hours <= 24):
-//         document.body.textContent = 'Добрый вечер';
-//     break;
-
-//     case (hours > 0 && hours <= 5 ):
-//         document.body.textContent = 'Доброй ночи';
-//     break;
-    
-//     default:
-//         document.body.textContent = 'Не то';
-
-// }
+// console.log(newYear);
+// console.log('seconds', seconds);
+// console.log('minutes', minutes);
+// console.log('hours', hours);
+// console.log('today', today);
+// console.log('time: ', time);
+// console.log('date', date);
 
 function out(hours){
     
@@ -48,10 +29,9 @@ function getWeekDay(day) {
     return days[day];
   }
 
-function ampm(hours){
-    if (hours >=0 && hours <12) return 'AM';
-    else return 'PM';
-}
+  function getTime(){
+      return date.toLocaleString('en-US',{hour: 'numeric', hour12: true, minute: 'numeric', second: 'numeric' });
+  }
 
 function countDays(){
     return Math.floor((newYear - date) / 86400000);
@@ -59,7 +39,14 @@ function countDays(){
 
 
 
-alert(`${out(hours)}
-Сегодня: ${getWeekDay(today)}
-Текущее время: ${hours}:${minutes}:${seconds} ${ampm(hours)}
-До нового года осталось: ${countDays()} дней`);
+document.querySelector('.welcome').textContent = out(hours);
+document.querySelector('.today').textContent = `Сегодня: ${getWeekDay(today)}`;
+document.querySelector('.timer').textContent = `Текущее время: ${getTime()}`;
+setInterval(() => {
+    document.querySelector('.timer').textContent = `Текущее время: ${getTime()}`;
+    timer();
+},1000);
+document.querySelector('.count').textContent = `До нового года осталось: ${countDays()} дней`;
+    }
+    timer();
+});
