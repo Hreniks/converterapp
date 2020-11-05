a9551982c91487d71b2cb0b15b12c4dd1836cc1e
hreniks <sbigor09@gmail.com>
2020-11-05 20:12:55 +0300

    ended lesson18

---
+++ script/script.js
@@ -0,0 +1,46 @@
+window.addEventListener('DOMContentLoaded', function(){
+    'use strict';
+    //timer
+    function countTimer(deadline){
+        let timerHours = document.querySelector('#timer-hours');
+        let timerMinutes = document.querySelector('#timer-minutes');
+        let timerSeconds = document.querySelector('#timer-seconds');
+        
+    function getTimeRemaining(){
+        let dateStop = new Date(deadline).getTime();
+        let dateNow = new Date().getTime();
+        let timeRemaining = (dateStop - dateNow) / 1000;
+        let seconds = Math.floor(timeRemaining % 60);
+        let minutes = Math.floor((timeRemaining / 60) % 60) ;
+        let hours = Math.floor(timeRemaining / 60 / 60) % 24;
+        
+        return {
+            timeRemaining,  
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+        }
+    function updateClock(){
+        let timer = getTimeRemaining();
+        if (timer.timeRemaining > 0){
+        timerHours.textContent = timer.hours < 10 ? '0' + timer.hours : timer.hours;
+        timerMinutes.textContent = timer.minutes < 10 ? '0' + timer.minutes : timer.minutes;
+        timerSeconds.textContent = timer.seconds < 10 ? '0' + timer.seconds : timer.seconds;
+        console.log(timer.timeRemaining);
+        }
+        else {
+            clearInterval(updateClock);
+            timerHours.textContent = '00';
+            timerMinutes.textContent = '00';
+            timerSeconds.textContent = '00';
+        }
+        
+    }
+    updateClock();
+    setInterval(updateClock,1000);
+}
+
+    countTimer('04 november 2020');
+    //setInterval(countTimer,1000,'06 november 2020');
+});
---
+++ script/Отдельная папка/js.js
@@ -0,0 +1,65 @@
+"use strict";
+let date = new Date();
+let seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+let minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+let hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+let today = date.getDay();
+let time = date.getTime();
+let newYear = new Date('January 1, 2021 00:00:00');
+console.log(newYear);
+console.log('seconds', seconds);
+console.log('minutes', minutes);
+console.log('hours', hours);
+console.log('today', today);
+console.log('time: ', time);
+console.log('date', date);
+//switch (hours){
+//     case (hours > 5 && hours <= 12):
+//         document.body.textContent = 'Доброе утро';
+//         break;
+
+//     case (hours > 12 && hours <= 16):
+//         document.body.textContent = 'Добрый день';
+//         break;
+
+//     case (hours > 16 && hours <= 24):
+//         document.body.textContent = 'Добрый вечер';
+//     break;
+
+//     case (hours > 0 && hours <= 5 ):
+//         document.body.textContent = 'Доброй ночи';
+//     break;
+    
+//     default:
+//         document.body.textContent = 'Не то';
+
+// }
+
+function out(hours){
+    
+    if (hours > 5 && hours <= 12) return 'Доброе утро';
+    if (hours > 12 && hours <= 16) return 'Добрый день';
+    if (hours > 16 && hours <= 23) return 'Добрый вечер';
+    if (hours > 0 && hours <= 5 ) return 'Доброй ночи';
+}
+
+function getWeekDay(day) {
+    let days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+    return days[day];
+  }
+
+function ampm(hours){
+    if (hours >=0 && hours <12) return 'AM';
+    else return 'PM';
+}
+
+function countDays(){
+    return Math.floor((newYear - date) / 86400000);
+}
+
+
+
+alert(`${out(hours)}
+Сегодня: ${getWeekDay(today)}
+Текущее время: ${hours}:${minutes}:${seconds} ${ampm(hours)}
+До нового года осталось: ${countDays()} дней`);
