24014d0264e14f028ecacde96c1f3febe3d16015
hreniks <sbigor09@gmail.com>
2020-11-08 04:02:55 +0300

    ended lesson20

--- script/script.js
+++ script/script.js
@@ -52,20 +52,38 @@ window.addEventListener('DOMContentLoaded', function () {
         const menu = document.querySelector('menu');
         const closeBtn = document.querySelector('.close-btn');
         const menuItems = menu.querySelectorAll('ul>li');
-
+        const menuBlock = document.querySelector('.menu-div');
+        
+      
 
         const handlerMenu = () => {
             menu.classList.toggle('active-menu');
         };
 
-        btnMenu.addEventListener('click', handlerMenu);
-        closeBtn.addEventListener('click', handlerMenu);
+        
 
-        for (let i = 0; i < menuItems.length; i++) {
-            menuItems[i].addEventListener('click', handlerMenu);
-        }
 
-        menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+        //btnMenu.addEventListener('click', handlerMenu);
+        document.body.addEventListener('click', (event) =>{
+            console.log(event.target);
+            
+            if (event.target.closest('.menu') || event.target.closest('menu a')){
+               handlerMenu();
+            }
+            else if (!event.target.closest('.menu') && menu.classList.contains('active-menu')){
+                handlerMenu(); 
+            }
+
+        });
+
+        
+        //closeBtn.addEventListener('click', handlerMenu);
+
+        // for (let i = 0; i < menuItems.length; i++) {
+        //     menuItems[i].addEventListener('click', handlerMenu);
+        // }
+
+        // menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
 
         //    menuItems[0].addEventListener('click', () => {
         //     document.querySelector('.service').scrollBy({behavior: 'smooth', block: 'start'});
@@ -114,52 +132,39 @@ window.addEventListener('DOMContentLoaded', function () {
         const popup = document.querySelector('.popup');
         const popupBtn = document.querySelectorAll('.popup-btn');
         const popUpClose = document.querySelector('.popup-close');
-
-
-
-        popupBtn.forEach((elem) => {
-            elem.addEventListener('click', () => {
-                //popup.style.display = 'block';
-                showPopup();
-
-            });
-        });
-
-        
-
-
+        const popupContent = document.querySelector('.popup-content');
+      
         popUpClose.addEventListener('click', () => {
-            popup.style.display = 'none';
-            
+          popup.style.display = 'none';
         });
-
+      
         const showPopup = () => {
-            const popup = document.querySelector('.popup');
-            const popupContent = document.querySelector('.popup-content');
-            popup.style.display = 'block'; // показать попап
-            if (screen.innerWidth > 768) { // если ширина экрана больше заданного числа, то запустить анимацию
-              let start = Date.now(); // получить стартовое время анимации (в момент клика)
-              let timer = setInterval(() => {
-                let timePassed = Date.now() - start; // запуск таймера, отнять от текущего реального времени стартовое время, после клика
-                if (timePassed >= 800) {
-                  clearInterval(timer); // если время достигло определенного числа удалить setInterval 
-                  return;
-                }
-                draw(timePassed); // отрисовка анимации 
-              });
-              let draw = (timePassed) => {
-                let wContent = +getComputedStyle(popupContent).width.split('px')[0]; // получить стили попап контента (блок с самой формой, а не вся обёртка, с попап )
-                wContent = -wContent / 2 + 50 + 'px'; // данные для центрирования по горизонтали
-                popupContent.style.left = timePassed / 16 + '%'; // центрирование по горизонтали
-                popupContent.style.marginLeft = wContent; // центрирование по горизонтали
-              };
-            }
-          };
-
-        // document.querySelector('.popup-content')
-
-    };
-    togglePopUp();
+          popup.style.display = 'block'; // показать попап
+          if (window.innerWidth > 768) { // если ширина экрана больше заданного числа, то запустить анимацию
+            let start = Date.now(); // получить стартовое время анимации (в момент клика)
+            let timer = setInterval(() => {
+              let timePassed = Date.now() - start; // запуск таймера, отнять от текущего реального времени стартовое время, после клика
+              if (timePassed >= 800) {
+                clearInterval(timer); // если время достигло определенного числа удалить setInterval 
+                return;
+              }
+              draw(timePassed); // отрисовка анимации 
+            });
+            let draw = (timePassed) => {
+              let wContent = +getComputedStyle(popupContent).width.split('px')[0]; // получить стили попап контента (блок с самой формой, а не вся обёртка, с попап )
+              wContent = -wContent / 2 + 50 + 'px'; // данные для центрирования по горизонтали
+              popupContent.style.left = timePassed / 16 + '%'; // центрирование по горизонтали
+              popupContent.style.marginLeft = wContent; // центрирование по горизонтали
+            };
+          }
+        };
+      
+        popupBtn.forEach((elem) => {
+          elem.addEventListener('click', showPopup);
+        });
+      
+      };
+      togglePopUp();
 
 
     //tabs
