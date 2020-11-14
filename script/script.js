2db73b469f1f08cb031f27928911c5c6955ea68e
hreniks <sbigor09@gmail.com>
2020-11-14 10:16:05 +0300

    ended lesson26

--- script/script.js
+++ script/script.js
@@ -371,6 +371,7 @@ window.addEventListener('DOMContentLoaded', () => {
             totalValue = document.getElementById('total'),
             calcCount = document.querySelector('.calc-count');
             let total = 0;
+            let interval;
 
         const countSum = () =>{
             
@@ -404,7 +405,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
             let n = 0;
             let t = Math.round(time / (total / step));
-            let interval = setInterval(() => {
+            interval = setInterval(() => {
                 n += step;
                 if (n === total){
                     clearInterval(interval);
@@ -420,17 +421,175 @@ window.addEventListener('DOMContentLoaded', () => {
         calcBlock.addEventListener('change',(e) => {
             const target = e.target;
             
-            if (target === calcType || target === calcSquare || target === calcDay || target === calcCount){
-                
+            if (target.matches('select') || target.matches('input')){
                 countSum();
                 
-                if (total !== 0 && calcType.value && calcSquare.value && calcDay.value && calcCount.value){
+                clearInterval(interval);
+                if (total !== 0){ 
                     calcAnim();
-                } 
-            }   
+                }
+            }  
         });
 
     };
 
     Calc(100);
+
+
+    //send-ajax-form
+
+
+    const sendForm = () => {
+        const errorMessage = `Что то пошло не так...`,
+        loadMessage = `Загрузка...`,
+        successMessage = 'Спасибо!, Мы скоро с вами свяжемся!';
+
+        const form1 = document.getElementById('form1');
+        const form2 = document.getElementById('form2');
+        const form3 = document.getElementById('form3');
+        const form1Inputs = form1.querySelectorAll('input');
+        const form2Inputs = form2.querySelectorAll('input');
+        const form3Inputs = form3.querySelectorAll('input');
+
+        const statusMessage = document.createElement('div');
+        //statusMessage.textContent = 'текст';
+        statusMessage.style.cssText = `font-size: 2rem`;
+        statusMessage.style.cssText = `color: #ffff`;
+
+        form1.addEventListener('submit', (event) => {
+            event.preventDefault();
+            form1.appendChild(statusMessage);
+            statusMessage.textContent = loadMessage;
+            
+            const form1Data = new FormData(form1);
+            let body = {};
+            form1Data.forEach((val,key) =>{
+                body[key] = val;
+            });
+            postData(body, () => {
+                statusMessage.textContent = successMessage;
+            }, (error) =>{
+                statusMessage.textContent = errorMessage;
+                console.log(error);
+            });
+
+            // if (form1.querySelector('.form-phone').value){
+            //     let re = /^\d[\d\(\)\ -]{4,14}\d$/;
+            //     let valid = re.test(form1.querySelector('.form-phone').value);
+            //     if (!valid){
+            //         statusMessage.textContent = 'Номер телефона введен не правильно!';
+            //     }
+            // }
+            // else {
+            //     event.preventDefault();
+            //     statusMessage.textContent = `Введите телефон!`;
+            // }
+
+          
+
+            form1Inputs.forEach((item) => {
+                console.log(item);
+                item.value = '';
+            });
+
+        });
+
+       const formValid = (name) => {
+           let form = document.getElementById(name);
+        form.addEventListener('input', (e) => {
+            if (e.target.classList.contains('form-phone')){
+                e.target.value = e.target.value.replace(/[^+0-9]/ig, '');
+            }
+            if (e.target.classList.contains('form-name')){
+                e.target.value = e.target.value.replace(/[^а-яё\s]/ig, '');
+            }
+
+            if (e.target.classList.contains('mess')){
+                e.target.value = e.target.value.replace(/[^а-яё\s]/ig, '');
+            }
+        });
+       };
+       formValid('form1');
+       formValid('form2');
+       formValid('form3');
+
+
+        form2.addEventListener('submit', (e) => {
+            e.preventDefault();
+            form2.appendChild(statusMessage);
+            statusMessage.textContent = loadMessage;
+
+            const form2Data = new FormData(form2);
+            let body = {};
+
+            form2Data.forEach((val,key) => {
+                body[key] = val;
+            });
+
+            console.log(body);
+
+            postData(body, () => {
+                statusMessage.textContent = successMessage;
+            }, (error) =>{
+                statusMessage.textContent = errorMessage;
+                console.log(error);
+            });
+
+
+            form2Inputs.forEach((item) => {
+                console.log(item);
+                item.value = '';
+            });
+        });
+
+
+        form3.addEventListener('submit', (e) => {
+            e.preventDefault();
+            form3.appendChild(statusMessage);
+            statusMessage.textContent = loadMessage;
+
+            const form3Data = new FormData(form3);
+            let body = {};
+
+            form3Data.forEach((val,key) => {
+                body[key] = val;
+            });
+
+            console.log(body);
+
+            postData(body, () => {
+                statusMessage.textContent = successMessage;
+            }, (error) =>{
+                statusMessage.textContent = errorMessage;
+                console.log(error);
+            });
+
+
+            form3Inputs.forEach((item) => {
+                console.log(item);
+                item.value = '';
+            });
+        });
+
+        const postData = (body, outputData,errorData) => {
+            const request = new XMLHttpRequest();
+            request.addEventListener('readystatechange', () => {
+                if (request.readyState !== 4){
+                    return;
+                }
+
+                if (request.status === 200) {
+                    outputData();
+                }
+                else {
+                    errorData(request.status);
+                }
+            });
+            request.open('POST', './server.php');
+            request.setRequestHeader('Content-Type','application/json');
+            
+            request.send(JSON.stringify(body));
+        };
+    };
+    sendForm();
 });
